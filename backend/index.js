5f1c0a1f3a4b4c6d90aa827a439cd4c4485bb414
Rohit Mondal <rohitmondal.22@nshm.edu.in>
2025-08-25 13:49:10 +0530

    cors changed

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,25 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://plant-store-brown.vercel.app" 
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // DB connection
@@ -34,5 +51,4 @@ app.use((err, req, res, next) => {
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  connectDB();
 });
